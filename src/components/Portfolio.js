aa657453f6a50e8e69fe051f9430743665c8bc0a
fernvilla <fvilla18@gmail.com>
2017-07-07 23:38:50 -0700

    Portfolio links

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,71 +13,101 @@ export default class Portfolio extends Component {
         <div className="row">
           <div className="col-md-4">
             <div className="project">
-              <img src="/images/panjo.jpg" className="img-fluid" alt="panjo" />
+              <a href="http://panjo.com">
+                <img
+                  src="/images/panjo.jpg"
+                  className="img-fluid"
+                  alt="panjo"
+                />
+              </a>
             </div>
           </div>
 
           <div className="col-md-4">
             <div className="project">
-              <img
-                src="/images/snla.jpg"
-                className="img-fluid"
-                alt="sports news la"
-              />
+              <a href="http://sportsnewsla.com">
+                <img
+                  src="/images/snla.jpg"
+                  className="img-fluid"
+                  alt="sports news la"
+                />
+              </a>
             </div>
           </div>
 
           <div className="col-md-4">
             <div className="project">
-              <img
-                src="/images/bloomtri.jpg"
-                className="img-fluid"
-                alt="bloomtri"
-              />
+              <a href="http://bloomtri.com">
+                <img
+                  src="/images/bloomtri.jpg"
+                  className="img-fluid"
+                  alt="bloomtri"
+                />
+              </a>
             </div>
           </div>
 
           <div className="col-md-4">
             <div className="project">
-              <img
-                src="/images/trubrain.jpg"
-                className="img-fluid"
-                alt="trubrain"
-              />
+              <a href="http://trubrain.com">
+                <img
+                  src="/images/trubrain.jpg"
+                  className="img-fluid"
+                  alt="trubrain"
+                />
+              </a>
             </div>
           </div>
 
           <div className="col-md-4">
             <div className="project">
-              <img
-                src="/images/wedding.jpg"
-                className="img-fluid"
-                alt="wedding"
-              />
+              <a href="http://afwedding.herokuapp.com">
+                <img
+                  src="/images/wedding.jpg"
+                  className="img-fluid"
+                  alt="wedding"
+                />
+              </a>
             </div>
           </div>
 
           <div className="col-md-4">
             <div className="project">
-              <img src="/images/snla2.jpg" className="img-fluid" alt="snla2" />
+              <a href="http://snla.herokuapp.com">
+                <img
+                  src="/images/snla2.jpg"
+                  className="img-fluid"
+                  alt="snla2"
+                />
+              </a>
             </div>
           </div>
 
           <div className="col-md-4">
             <div className="project">
-              <img src="/images/gloss.jpg" className="img-fluid" alt="gloss" />
+              <a href="http://cracked.herokuapp.com">
+                <img
+                  src="/images/gloss.jpg"
+                  className="img-fluid"
+                  alt="gloss"
+                />
+              </a>
             </div>
           </div>
 
           <div className="col-md-4">
             <div className="project">
-              <img src="/images/map.jpg" className="img-fluid" alt="map" />
+              <a href="http://devjobs-map.herokuapp.com">
+                <img src="/images/map.jpg" className="img-fluid" alt="map" />
+              </a>
             </div>
           </div>
 
           <div className="col-md-4">
             <div className="project">
-              <img src="/images/gym.jpg" className="img-fluid" alt="gym" />
+              <a href="http://gym-time.herokuapp.com">
+                <img src="/images/gym.jpg" className="img-fluid" alt="gym" />
+              </a>
             </div>
           </div>
         </div>
